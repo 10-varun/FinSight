46b7ccea1a9c8c2de9bce7ee5e23718fc56c3ef2
yadu <yadunandanv08@gmail.com>
2025-02-23 21:41:39 +0530

    added loaders

--- frontend/src/components/Summary.js
+++ frontend/src/components/Summary.js
@@ -100,9 +100,13 @@ function Summary({ summary, overallScore, investmentAdvice, error, isLoading }) 
   return (
     <section className="summary-container">
       {isLoading ? (
-        <div className="loading-indicator">
-          <div className="spinner"></div>
-          <p>Loading company data...</p>
+        <div className="loading-indicator"> 
+          <svg viewBox="0 0 240 240" height="240" width="240" class="pl">
+            <circle stroke-linecap="round" stroke-dashoffset="-330" stroke-dasharray="0 660" stroke-width="20" stroke="#000" fill="none" r="105" cy="120" cx="120" class="pl__ring pl__ring--a"></circle>
+            <circle stroke-linecap="round" stroke-dashoffset="-110" stroke-dasharray="0 220" stroke-width="20" stroke="#000" fill="none" r="35" cy="120" cx="120" class="pl__ring pl__ring--b"></circle>
+            <circle stroke-linecap="round" stroke-dasharray="0 440" stroke-width="20" stroke="#000" fill="none" r="70" cy="120" cx="85" class="pl__ring pl__ring--c"></circle>
+            <circle stroke-linecap="round" stroke-dasharray="0 440" stroke-width="20" stroke="#000" fill="none" r="70" cy="120" cx="155" class="pl__ring pl__ring--d"></circle>
+          </svg>
         </div>
       ) : error ? (
         <p className="error">{error}</p>
